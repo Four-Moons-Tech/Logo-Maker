0c80f418ff51fc9badd602bc829b141eb13d05ac
Anna Moon <anna@fourmoonstech.com>
2023-12-08 18:58:16 -0600

    edited shapes file to inlude set and render methods,edited generatelogo to dry the code to have one function for 3 shapes instead of 3 separate, included if statement for shape choice, installed jest

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-console.log('hello')
+const inquirer = require('inquirer');
+const generateSVG = require('./lib/generatelogo.js')
+const { Triangle, Circle, Square } = require('./lib/shapes.js')
+
+
+inquirer
+    .prompt([
+        { /* Pass your questions in here */
+            name: 'characters',
+            type: 'input',
+            message: 'Enter characters for your logo',
+        },
+        {
+            name: 'characterColor',
+            type: 'input',
+            message: 'What color do you want for your characters for your logo?',
+        },
+        {
+            type: "list",
+            name: "shapeChoices",
+            message: "Choose Logo shape",
+            choices:
+                ["Circle",
+                    "Square",
+                    "Triangle",
+                ]
+        },
+        {
+            name: 'shapeColor',
+            type: 'input',
+            message: 'Enter shape color',
+        },
+    ])
+    .then((answers) => {
+
+        const { characters, characterColor, shapeChoices, shapeColor } = answers;
+
+
+        if (characters.length > 3) {
+            console.log('Please enter up to 3 characters')
+        }
+        let logoShape;
+
+        if (shapeChoices == "Triangle") {
+            logoShape = new Triangle()
+        } else if (shapeChoices == "Circle") {
+            logoShape = new Circle()
+        } else if (shapeChoices == "Square") {
+            logoShape = new Square()
+        }
+        logoShape.setText(characters)
+        logoShape.setShapeColor(shapeColor)
+        logoShape.setTextColor(characterColor)
+
+
+        return generateSVG(logoShape.render(), shapeChoices)
+
+
+
+
+    })
+
+
+
